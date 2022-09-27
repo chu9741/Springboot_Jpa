47b72c2d0d6cb81fd7b7792398bb45957f1d5be5
chu9741 <chu9741@gmail.com>
2022-09-27 22:31:07 +0900

    상품 엔티티 비즈니스 로직 추가

--- src/main/java/com/example/jpashop/domain/item/Item.java
+++ src/main/java/com/example/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.jpashop.domain.item;
 
 import com.example.jpashop.domain.Category;
+import com.example.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,5 +27,19 @@ public abstract class Item { // 상속 클래스로 만드니까 abs
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //======비즈니스 로직======//
+
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity -quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 
 }
