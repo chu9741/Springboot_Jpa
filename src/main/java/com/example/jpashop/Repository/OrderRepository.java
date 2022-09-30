0266e6ceb7c5bc56dd1fcdc1d4e6804f86df88e9
chu9741 <chu9741@gmail.com>
2022-09-30 23:53:35 +0900

    OrderRepo를 수정(interface -> @Repo를 사용)

--- src/main/java/com/example/jpashop/Repository/OrderRepository.java
+++ src/main/java/com/example/jpashop/Repository/OrderRepository.java
@@ -1,18 +1,54 @@
 package com.example.jpashop.Repository;
 
 import com.example.jpashop.domain.Order;
+import com.example.jpashop.domain.OrderStatus;
+import com.example.jpashop.domain.QMember;
+import com.example.jpashop.domain.QOrder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.criteria.internal.predicate.BooleanExpressionPredicate;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
-//@Repository
+import static com.example.jpashop.domain.QMember.member;
+import static com.example.jpashop.domain.QOrder.order;
+
+@Repository
 @RequiredArgsConstructor
 public class OrderRepository {
 
+    @PersistenceContext
     private final EntityManager em;
+    JPAQueryFactory query = new JPAQueryFactory(em);
+    public List<Order> findAll(OrderSearch orderSearch){
+        QMember member = QMember.member;
+        QOrder order = QOrder.order;
 
+
+        return query.select(order).from(order)
+                .join(order.member,member)
+                .where(statusEq(orderSearch.getOrderStatus())
+                        ,nameLike(orderSearch.getMemberName()))
+                .limit(1000).fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond){
+        if(statusCond == null){
+            return null;
+        }
+        return order.status.eq(statusCond);
+    }
+    private BooleanExpression nameLike(String nameCond){
+        if (!StringUtils.hasText(nameCond)){
+            return null;
+        }
+        return member.name.like(nameCond);
+    }
     public void save(Order order){
         em.persist(order);
     }
---
+++ src/main/java/com/example/jpashop/Repository/OrderSearch.java
@@ -0,0 +1,13 @@
+package com.example.jpashop.Repository;
+
+import com.example.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus; //주문상태 [ORDER, CANCEL]
+}
